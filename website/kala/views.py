f4c1fea6d542655191fb6b0243aeaef32f4a5728
p c <palak.chopra@somaiya.edu>
2021-06-02 21:00:29 +0530

    Add existing file

--- website/kala/views.py
+++ website/kala/views.py
@@ -7,8 +7,7 @@ from django.core.mail import message, send_mail
 # Create your views here.
 
 def home(request):
-    prod = Product.objects.all()  
-    print({'prod' : prod}) 
+    prod = Product.objects.all()   
     return render(request,'home.html',{'prod' : prod})
 
 def about(request):
@@ -36,8 +35,7 @@ def sell(request):
   
         if form.is_valid():
             form.save()
-            prod = Product.objects.order_by('-id')[:1]
-            print({'prod' : prod}) 
+            prod = Product.objects.order_by('-id')[:1] 
             return render(request,'product.html',{'prod':prod[0]}) 
     else:
         form = ProductForm()
@@ -62,6 +60,8 @@ def checkout(request,prodid):
             subject = 'Order Placed!'
             message = 'Your order of {0} has been placed\nOrder number: {1}\nThank you for shopping with us and supporting independant artists!'.format(prod[0],form_f.id) 
             send_mail(subject,message,'shopart.kala@gmail.com',[customer_email])
+            return HttpResponse("Order Placed!")
+    
     else:
         form = CheckoutForm()
     return render(request, 'checkout.html', {'form' : form,'prod':prod[0]})
